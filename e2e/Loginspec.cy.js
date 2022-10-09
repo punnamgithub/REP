f50052464f81df7230d74e452db4d605e4d3d299
punnamgithub <112564727+punnamgithub@users.noreply.github.com>
2022-10-09 09:30:18 +0530

    Add files via upload

---
+++ e2e/Loginspec.cy.js
@@ -0,0 +1,16 @@
+describe('Test Spec', () => {
+  it('Test Pass', () => {
+    
+    cy
+  .fixture('Test')
+  .then( Test => {
+    expect(Test.color).to.eq("red")
+    expect(Test.id).to.eq(4)
+    expect(Test.available).to.eq(false)
+
+    
+  }
+  )
+
+  })
+})
